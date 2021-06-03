db50574955f8186d4cd75e75ebf473a625c11a50
Fenringer <example@example.com>
2021-06-03 21:53:30 +0300

    solved

--- 27(2).sql
+++ 27(2).sql
@@ -2,18 +2,25 @@
 Найдите средний размер диска ПК каждого из тех производителей, которые выпускают и принтеры. 
 Вывести: maker, средний размер HD.
 */
---NOT SOLVED
+ 
+--I вариант
+select maker, avg(hd) as avg_hd
+from product join pc on product.model=pc.model
+where maker in (select maker from product where type = 'printer')
+group by maker
+
+--II вариант
+/*
 select maker, avg(hd)
 from (
-      select maker, hd
-      from pc join product on pc.model=product.model
-      where maker in 
-(
-select maker
-from product 
-join printer on printer.model=product.model 
-where product.type = 'printer'
-group by maker
-)
+	select maker, hd
+	from pc join product on pc.model=product.model
+	where maker in 
+		(
+		select maker
+		from product
+		where product.type = 'printer'
+		)
      ) result
 group by maker
+*/
