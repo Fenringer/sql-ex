396006a8d5d8026d49cf4418180ec4a1f313cfaa
Fenringer <example@example.com>
2021-06-03 21:55:21 +0300

    solved

--- 25(2).sql
+++ 25(2).sql
@@ -3,13 +3,40 @@
 и с самым быстрым процессором среди всех ПК, имеющих наименьший объем RAM. 
 Вывести: Maker
 */
---UNSOLVED
 
-SELECT * FROM 
-			(SELECT * FROM Product JOIN (
-					SELECT model m , speed, ram, price
-					FROM pc	
-					WHERE ram IN (SELECT MIN(ram)FROM pc)) a 
-					ON Product.model = a.m) b
-WHERE speed IN (SELECT MAX(speed) FROM b)
+-- I вариант
+-- cost: 0.040868349373341
+WITH io AS
+(SELECT maker, Product.model, speed, ram
+FROM Product JOIN PC ON Product.model = PC.model 
+AND maker IN (SELECT maker FROM Product WHERE type = 'Printer') 
+AND ram = (SELECT MIN(ram) FROM PC))
+SELECT DISTINCT maker FROM io WHERE speed = (SELECT MAX(speed) FROM io)
+
+-- II вариант
+--cost: 0.048855666071177
+/*SELECT DISTINCT maker
+FROM Product 
+WHERE type = 'printer' 
+	AND maker IN(
+		SELECT maker 
+		FROM Product
+		WHERE model IN(
+			SELECT model 
+			FROM PC
+			WHERE speed = (
+				SELECT MAX(speed)
+				FROM (
+					SELECT speed 
+					FROM PC 
+					WHERE ram=(
+						SELECT MIN(ram)
+						FROM PC
+						)
+				) AS z4
+			)
+			and ram=(SELECT MIN(ram) FROM PC)
+
+		)
+);*/
 
